3555debb956ddc66a28ba873cf592c2d4df56e07
Urpagin <passieuxmattis@gmail.com>
2023-05-24 04:17:08 +0200

    more things

--- src/routes/random-list/Card.svelte
+++ src/routes/random-list/Card.svelte
@@ -3,22 +3,11 @@
   </div>
   
   <style>
-    /* .card {
-      background-color: #fff;
-      color: #333;
-      border-radius: 15px;
-      padding: 40px 50px;
-      margin: 20px 0;
-      position: relative;
-    } */
 
     .card {
       background-color: #ba279a;
         padding-top: 20px;
         padding-bottom: 20px;
-        /* margin-top: 1%;
-        margin-right: 10%;
-        margin-left: 30%; */
         border-radius: 20px;
     }
   </style>
